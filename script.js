abca73787d2ca8c5c5a32641c8b7e8b925318a85
Stella Anđelić <sandelic@hattrick.local>
2021-10-14 13:06:05 +0200

    Fetch data

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+let kittens;
+
+async function loadJSON(path) {
+    const data = await fetch(path);
+    return data.json();
+}
+
+async function main() {
+    kittens = await loadJSON('/kittens.json');
+    console.log(kittens);
+}
+
+main();
