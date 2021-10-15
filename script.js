39e965a2fe5d886b0c42bf700949a1a25c54ed1c
Stella Anđelić <sandelic@hattrick.local>
2021-10-15 10:43:59 +0200

    Basic carousel functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let kittens;
+/* Fetch data */
 
 async function loadJSON(path) {
     const data = await fetch(path);
@@ -7,7 +7,53 @@ async function loadJSON(path) {
 
 async function main() {
     kittens = await loadJSON('/kittens.json');
-    console.log(kittens);
+    localStorage.setItem('kittens', JSON.stringify(kittens))
 }
 
 main();
+
+let kittens = JSON.parse(localStorage.getItem('kittens'));
+
+
+/* Carousel logic */
+
+function getYoungestN(n) {
+    let sorted = kittens.sort((a, b) => {
+        return a.age - b.age;
+    });
+    let youngestN = sorted.slice(0, n);
+    return youngestN;
+}
+
+let youngestFour = getYoungestN(4);
+console.log(youngestFour);
+
+let carouselSlide = document.getElementById('slide');
+let initImg = document.createElement('img');
+initImg.src = youngestFour[0].image;
+initImg.alt = youngestFour[0].alt;
+carouselSlide.appendChild(initImg);
+
+let arrowLeft = document.getElementById('left');
+let arrowRight = document.getElementById('right');
+
+let ctr = 1;
+
+arrowRight.addEventListener('click', function () {
+    if (ctr == youngestFour.length) ctr = 0;
+    let carouselSlide = document.getElementById('slide');
+    let img = carouselSlide.children[0];
+    img.src = youngestFour[ctr].image;
+    img.alt = youngestFour[ctr].name;
+    ctr = ctr + 1;
+});
+
+arrowLeft.addEventListener('click', function () {
+    ctr = ctr - 1;
+    if (ctr == -1) ctr = youngestFour.length - 1;
+    console.log(youngestFour[ctr]);
+    let carouselSlide = document.getElementById('slide');
+    let img = carouselSlide.children[0];
+    img.src = youngestFour[ctr].image;
+    img.alt = youngestFour[ctr].name;
+});
