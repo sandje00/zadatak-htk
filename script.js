b65a94b05983db0886aed010fda20d088ada6397
Stella Anđelić <sandelic@hattrick.local>
2021-10-20 13:25:24 +0200

    Rewrite carousel logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import CarouselController from './js/carouselController.js';
 import Kittens from './js/kittens.js';
 
 /* Fetch data */
@@ -16,40 +17,37 @@ main();
 
 const kittens = new Kittens(JSON.parse(localStorage.getItem('kittens')));
 
-/* Carousel logic */
-    
-let youngestFour = kittens.getTopN(4, 'age');
-    
-let carouselSlide = document.getElementById('slide');
-let initImg = document.createElement('img');
-initImg.src = youngestFour[0].image;
-initImg.alt = youngestFour[0].alt;
-carouselSlide.appendChild(initImg);
-    
-let arrowLeft = document.getElementById('left');
-let arrowRight = document.getElementById('right');
-    
-let ctr = 1;
-    
-arrowRight.addEventListener('click', function () {
-    if (ctr == youngestFour.length) ctr = 0;
-    let carouselSlide = document.getElementById('slide');
-    let img = carouselSlide.children[0];
-    img.src = youngestFour[ctr].image;
-    img.alt = youngestFour[ctr].name;
-    ctr = ctr + 1;
-});
-    
-arrowLeft.addEventListener('click', function () {
-    ctr = ctr - 1;
-    if (ctr == -1) ctr = youngestFour.length - 1;
-    let carouselSlide = document.getElementById('slide');
-    let img = carouselSlide.children[0];
-    img.src = youngestFour[ctr].image;
-    img.alt = youngestFour[ctr].name;
-});
-    
 
+/* Carousel logic */
+
+const carouselController = new CarouselController(kittens.getTopN(4, 'age'));
+
+loadCarousel();
+
+let arrows = document.querySelectorAll('.kitten-carousel-button');
+arrows.forEach(arrow => arrow.addEventListener('click', e => onArrowClick(e)));
+
+function onArrowClick(e) {
+    console.log(e.target);
+    const arrow = e.target;
+    if (arrow.classList.contains('right'))
+        loadCarousel(carouselController.moveRight());
+    if (arrow.classList.contains('left'))
+        loadCarousel(carouselController.moveLeft());
+}
+
+function loadCarousel() {
+    let carouselSlide = document.getElementById('slide');
+    let currentItem = carouselController.getCurrentItem();
+    carouselSlide.innerHTML += createCarouselSlide(currentItem);
+}
+
+function createCarouselSlide(item) {
+    return `<img src=${item.image} alt=${item.alt}>`;
+}
+
+/** Search dashboard logic **/
+    
 const NUMBER_OF_ENTRIES = 4;
 let visibleKittensInitial = kittens.getTopN(NUMBER_OF_ENTRIES, 'age');
 renderVisibleKittens(visibleKittensInitial);
