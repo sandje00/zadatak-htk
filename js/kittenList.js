9b42b4585ff114948b9aead40a8e8c166e421d26
sandje00 <stella.andjelic.00@fesb.hr>
2021-12-10 10:00:39 +0100

    Fix kitten adoption bug

--- js/kittenList.js
+++ js/kittenList.js
@@ -8,22 +8,20 @@ class KittenList {
     constructor(entries, action) {
         this.entries = entries;
         this.action = action;
+        this.initListItemsNum = 20;
+        this.appliedFilters = [];
+        this.currentSortBy = 'age';
+        this.currentSortOrder = 'asc';
+        this.visibleEntries = numericSort(this.entries, 'age', true);
         this.searchList = document.getElementById('kitten-list');
         this.searchBox = document.getElementById('kitten-search-box');
         this.showMoreButton = document.getElementById('show-more');
         this.radioSortBy = document.querySelectorAll('input[type=radio][name="sort-by"]');
         this.radioSortOrder = document.querySelectorAll('input[type=radio][name="sort-order"]');
         this.checkboxFilter = document.querySelectorAll('input[type=checkbox]');
-        this.initListItemsNum = 20;
-        this.appliedFilters = [];
-        this.history = [];
-        this.visibleEntries = numericSort(this.entries, 'age', true);
-        this.currentSortBy = 'age';
-        this.currentSortOrder = 'asc';
     }
 
     init() {
-        this._updateHistory([...this.visibleEntries], null);
         this.renderVisibleKittens(this.getTopN(this.initListItemsNum, 'age'));
         this.searchBox.addEventListener('input', e => debounce(e => this._onSearch(e), 300)(e));
         this.showMoreButton.addEventListener('click', () => this._onShowMore());
@@ -37,31 +35,6 @@ class KittenList {
         });
         this.checkboxFilter.forEach(checkbox => checkbox.addEventListener('change', e => this._onFilterValueChange(e)));
     }
-    
-    sortByKey(key, order) {
-        const isAscending = order === 'asc' || !(order === 'desc');
-        if (key == 'age') this.visibleEntries = numericSort(this.visibleEntries, 'age', isAscending);
-        if (key == 'name') this.visibleEntries = alphabeticSort(this.visibleEntries, 'name', isAscending);
-        return this.visibleEntries;
-    }
-    
-    filterByKey(key, value) {
-        this._updateHistory([...this.visibleEntries], key + '-' + value);
-        if (key === 'age') this.visibleEntries = filterLessEqual(this.visibleEntries, 'age', value);
-        if (key === 'color') this.visibleEntries = filterEqual(this.visibleEntries, 'color', value);;
-        return this.visibleEntries;
-    }
-    
-    searchByKey(key, keyword) {
-        return filterByKeyword(this.visibleEntries, key, keyword);
-    }
-
-    getTopN(n, key) {
-        let sorted;
-        if (key == 'age') sorted = numericSort(this.entries, 'age', true);
-        if (key == 'name') sorted = alphabeticSort(this.entries, 'name', true);
-        return n <= sorted.length && sorted.slice(0, n);
-    }
 
     renderVisibleKittens(kittens) {
         if (this.searchList.hasChildNodes()) this.searchList.innerHTML = '';
@@ -70,19 +43,6 @@ class KittenList {
             this.searchList.appendChild(el);
         });
         return this;
-    }
-
-    removeEntry(id) {
-        this.visibleEntries = filterNonMatches(this.visibleEntries, 'id', id);
-        this.entries = filterNonMatches(this.entries, 'id', id);
-    }
-    
-    removeFilter(key, value) {
-        const index = this.appliedFilters.lastIndexOf(key + '-' + value);
-        if (index === -1) return;
-        if (index === this.appliedFilters.length - 1) this._removeLastFilter();
-        else this._removeInnerFilter(index);
-        return this.visibleEntries;
     }
 
     hideShowMoreButton() {
@@ -95,9 +55,21 @@ class KittenList {
         }
     }
 
+    removeEntry(id) {
+        this.visibleEntries = filterNonMatches(this.visibleEntries, 'id', id);
+        this.entries = filterNonMatches(this.entries, 'id', id);
+    }
+
+    getTopN(n, key) {
+        let sorted;
+        if (key == 'age') sorted = numericSort(this.entries, 'age', true);
+        if (key == 'name') sorted = alphabeticSort(this.entries, 'name', true);
+        return n <= sorted.length && sorted.slice(0, n);
+    }
+
     _onSearch(e) {
         let keyword = e.target.value.toUpperCase();
-        updateKittenList(e.target, () => this.searchByKey('name', keyword));
+        updateKittenList(e.target, () => this._searchByKey('name', keyword));
     }
 
     _onShowMore() {
@@ -106,49 +78,56 @@ class KittenList {
 
     _onSortByValueChange(e) {
         this.currentSortBy = e.target.value;
-        updateKittenList(e.target, () => this.sortByKey(this.currentSortBy, this.currentSortOrder));
+        updateKittenList(e.target, () => this._sortByKey(this.currentSortBy, this.currentSortOrder));
     }
 
     _onSortOrderValueChange(e) {
         this.currentSortOrder = e.target.value;
-        updateKittenList(e.target, () => this.sortByKey(this.currentSortBy, this.currentSortOrder));
+        updateKittenList(e.target, () => this._sortByKey(this.currentSortBy, this.currentSortOrder));
     }
 
     _onFilterValueChange(e) {
-        if (e.target.checked) updateKittenList(e.target, () => this.filterByKey(e.target.name, e.target.value));
-        else updateKittenList(e.target, () => this.removeFilter(e.target.name, e.target.value));
+        if (e.target.checked) updateKittenList(e.target, () => this._applyFilter(e.target.name, e.target.value));
+        else updateKittenList(e.target, () => this._removeFilter(e.target.name, e.target.value));
     }
 
-    _updateHistory(items, filterName) {
-        this.history.push(items);
-        this.appliedFilters.push(filterName);
+    _searchByKey(key, keyword) {
+        return filterByKeyword(this.visibleEntries, key, keyword);
+    }
+    
+    _sortByKey(key, order) {
+        const isAscending = order === 'asc' || !(order === 'desc');
+        if (key == 'age') this.visibleEntries = numericSort(this.visibleEntries, 'age', isAscending);
+        if (key == 'name') this.visibleEntries = alphabeticSort(this.visibleEntries, 'name', isAscending);
+        return this.visibleEntries;
     }
 
-    _removeLastFilter() {
-        this.appliedFilters.pop();
-        this.visibleEntries = this.history.pop();
+    _applyFilter(key, value) {
+        this.appliedFilters.push(key + '-' + value);
+        return this._filterByKey(key, value);
     }
 
-    _removeInnerFilter(index) {
-        this.history.splice(index);
-        this.visibleEntries = this.history[this.history.length - 1];
-        const rest = this.appliedFilters.splice(index);
-        rest.shift();
-        const filters = [];
-        rest.forEach(it => {
-            let filter = {};
-            let keyValue = it.split('-');
-            filter.key = keyValue[0];
-            filter.value = keyValue[1];
-            filters.push(filter);
+    _removeFilter(key, value) {
+        const index = this.appliedFilters.indexOf(key + '-' + value);
+        if (index === -1) return;
+        this.appliedFilters.splice(index, 1);
+        return this._applyMultipleFilters();
+    }
+    
+    _filterByKey(key, value) {
+        if (key === 'age') this.visibleEntries = filterLessEqual(this.visibleEntries, 'age', value);
+        if (key === 'color') this.visibleEntries = filterEqual(this.visibleEntries, 'color', value);
+        return this.visibleEntries;
+    }
+
+    _applyMultipleFilters() {
+        this.visibleEntries = this.entries;
+        if (this.appliedFilters.length) this.appliedFilters.forEach(filter => {
+            let [ key, value ] = filter.split('-');
+            this.visibleEntries = this._filterByKey(key, value);
         });
-        this._applyMultipleFilters(filters);
-    }
-
-    _applyMultipleFilters(filters) {
-        filters.forEach(({ key, value }) => { this.visibleEntries = this.filterByKey(key, value); });
-        this.visibleEntries = this.sortByKey(this.currentSortBy, this.currentSortOrder);
-        this.renderVisibleKittens(this.visibleEntries);
+        this.visibleEntries = this._sortByKey(this.currentSortBy, this.currentSortOrder);
+        return this.visibleEntries;
     }
 }
 
